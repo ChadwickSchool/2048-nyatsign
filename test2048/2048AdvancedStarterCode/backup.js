da0970a50ffd2095d27009a5cf0d9bfcff05bab7
frank glantz <frank glantz>
2016-12-28 14:01:05 -0800

    test advanced2048 file

---
+++ test2048/2048AdvancedStarterCode/backup.js
@@ -0,0 +1,364 @@
+var grid = [];
+var UP_ARROW = 38;
+var DOWN_ARROW = 40;
+var LEFT_ARROW = 37;
+var RIGHT_ARROW = 39;
+var move;
+var tileX;
+var tileY;
+var totalTileNumber;
+var board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+//As soon as webpage loads run these two functions
+/*$(document).ready(function(){
+    setUpBoard();
+    printBoard();
+    console.log("Loaded webpage"); //how you do print statements in javascript
+});
+*/
+function setUpBoard(){
+//var board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    // initialize board to have no values
+    for(var i=0; i<4; i++){
+        var innergrid = [];
+        for(var j=0; j<4; j++){
+          //  innergrid.push("x");
+          var boardID = "r"+i+"c"+j;
+			if(board[i][j]!=0){
+				document.getElementById(boardID).innerHTML = board[i][j];
+			}
+        }
+        grid.push(innergrid);
+    }
+    
+   addTile();
+    
+}
+
+function addTile() {
+    //place a 2 on a random spot in the board
+  //EXAMPLE:   if(parceInt(grid[r][j])<2){}
+    var x = Math.round(Math.random()*3);
+    var y = Math.round(Math.random()*3);
+    
+    while(board[x][y] !== 0){
+var x = Math.round(Math.random()*3);
+    var y = Math.round(Math.random()*3);
+    }
+board[x][y] = 2;
+
+}
+    
+
+function printBoard(){
+    var board = '<br/>' + "*--------------*" + '<br/>';
+    for(var i=0; i<grid.length; i++){
+        board += "|   ";
+        for(var j=0; j<grid[i].length; j++){
+            board += grid[i][j] + "   |   ";
+        }
+        board += '<br/>';
+        board += "*--------------*";
+        board += '<br/>';
+    }
+    
+    //console.log(board);
+    document.getElementById("container").innerHTML = board;
+}
+
+
+//function gets called anytime  a key is pressed 
+//e is a special variable
+// that references the event obeject that reads if the user is interacting with 
+//the window 
+document.onkeydown = function(e) {
+     
+    //makes it work in internet explorer which uses window.event and not e 
+    e = e || window.event;
+
+    //keyCode is actually a character value which we convert to a String
+    //to use triple equals sign 
+    if (e.keyCode == UP_ARROW) {
+        // up arrow
+
+        moveTilesUp();
+        combineTilesUp();
+        addTile();
+        console.log("Pressed up");
+        
+    }
+    //double equals sign will convert it for us 
+    else if (e.keyCode == DOWN_ARROW) {
+        // down arrow
+        
+        moveTilesDown();
+        combineTilesDown();
+        addTile();
+        console.log("Pressed down");
+
+    }
+    else if (e.keyCode == LEFT_ARROW) {
+       // left arrow
+       moveTilesLeft();
+       combineTilesLeft();
+       addTile();
+       console.log("Pressed left");
+    }
+    else if (e.keyCode == RIGHT_ARROW) {
+       // right arrow
+       moveTilesRight();
+       combineTilesRight();
+       addTile();
+       console.log("Pressed right");
+
+    //   console.log(grid.length);
+    } 
+  //  if()
+    
+    printBoard(); //have to recall print board to get the board to update
+};
+function combineTilesUp()
+
+{
+     for(var r=0; r < grid.length; r++)
+    {
+        for(var c=0; c<grid[r].length; c++)
+        {
+   if(r !== 0 && grid[r][c] !== "x" && grid[r-1][c] !== "x" && grid[r][c] === grid[r-1][c]){
+            totalTileNumber = parseInt(grid[r-1][c]) + parseInt(grid[r][c]);
+            console.log(totalTileNumber);
+
+            grid[r-1][c] = grid[r][c];
+
+            grid[r-1][c] = totalTileNumber;
+            if(grid[r][c] !== "x"){
+            grid[r][c] = "x";
+        }
+
+           // grid[r-1][c] = totalTileNumber;
+          // totalTileNumber = parseInt(grid[r-1][c]);
+}
+}
+}
+}
+function moveTilesUp()
+//ROWS ACROSS THE LEFT SIDE DOWNWARD, COLUMNS ACROSS THE TOP GOING TO RIGHT
+{
+    
+    for(var r=0; r < grid.length; r++)
+    {
+        for(var c=0; c<grid[r].length; c++)
+        {
+           
+            if(r !== 0  && grid[r][c] !== "x" && grid[r-1][c] === "x")
+            {
+                grid[r-1][c] = grid[r][c];
+                grid[r][c] = "x";
+           //     console.log(r);
+               //console.log(grid.length);
+            }
+            
+        }
+        
+    }   
+    
+}
+function combineTilesDown(){
+    for(var r=3; r >=0; r--)
+        
+    {
+
+        for(var c=0; c<grid[r].length; c++)
+        {
+    if(r !== 3 && grid[r][c] !== "x" && grid[r+1][c] !== "x" && grid[r][c] === grid[r+1][c]){
+            totalTileNumber = parseInt(grid[r+1][c]) + parseInt(grid[r][c]);
+            console.log(totalTileNumber);
+
+            grid[r+1][c] = grid[r][c];
+
+            grid[r+1][c] = totalTileNumber;
+            if(grid[r][c] !== "x"){
+            grid[r][c] = "x";
+        }
+}
+}
+           // grid[r-1][c] = totalTileNumber;
+          // totalTileNumber = parseInt(grid[r-1][c]);
+}
+}
+function moveTilesDown()
+//ROWS ACROSS THE LEFT SIDE DOWNWARD, COLUMNS ACROSS THE TOP GOING TO RIGHT
+{
+    
+    for(var r=3; r >=0; r--)
+        
+    {
+
+        for(var c=0; c<grid[r].length; c++)
+        {
+
+
+            if(r !== 3  && grid[r][c] !== "x" && grid[r+1][c] === "x")
+            {
+               // moveOnce = true;
+               
+                grid[r+1][c] = grid[r][c];
+                grid[r][c] = "x";
+            //   console.log(r);
+                
+                 }
+            }
+            
+        }
+        
+    }   
+    function combineTilesRight(){
+        for(var r=0; r < grid.length; r++)
+    {
+        for(var c=3; c >=0; c--)
+        {
+         if(c !== 3 && grid[r][c] !== "x" && grid[r][c+1] !== "x" && grid[r][c] === grid[r][c+1]){
+            totalTileNumber = parseInt(grid[r][c+1]) + parseInt(grid[r][c]);
+            console.log(totalTileNumber);
+
+            grid[r][c+1] = grid[r][c];
+
+            grid[r][c+1] = totalTileNumber;
+            if(grid[r][c] !== "x"){
+            grid[r][c] = "x";
+        }
+    }
+}
+
+           // grid[r-1][c] = totalTileNumber;
+          // totalTileNumber = parseInt(grid[r-1][c]);
+}
+    }
+function moveTilesRight()
+//ROWS ACROSS THE LEFT SIDE DOWNWARD, COLUMNS ACROSS THE TOP GOING TO RIGHT
+{
+    
+    for(var r=0; r < grid.length; r++)
+    {
+        for(var c=3; c >=0; c--)
+        {
+             
+            if(c !== 4  && grid[r][c] !== "x" && grid[r][c+1] === "x")
+            {
+                grid[r][c+1] = grid[r][c];
+                grid[r][c] = "x";
+         //       console.log(c);
+               //console.log(grid.length);
+            }
+            
+        }
+        
+    }   
+    
+}
+function combineTilesLeft(){
+     for(var r=0; r < grid.length; r++)
+    {
+        for(var c=0; c <=3; c++)
+        {
+     if(c !== 0 && grid[r][c] !== "x" && grid[r][c-1] !== "x" && grid[r][c] === grid[r][c-1]){
+            totalTileNumber = parseInt(grid[r][c-1]) + parseInt(grid[r][c]);
+            console.log(totalTileNumber);
+
+            grid[r][c-1] = grid[r][c];
+
+            grid[r][c-1] = totalTileNumber;
+            if(grid[r][c] !== "x"){
+            grid[r][c] = "x";
+        }
+}
+}
+           // grid[r-1][c] = totalTileNumber;
+          // totalTileNumber = parseInt(grid[r-1][c]);
+}
+}
+function moveTilesLeft()
+//ROWS ACROSS THE LEFT SIDE DOWNWARD, COLUMNS ACROSS THE TOP GOING TO RIGHT
+{
+    
+    for(var r=0; r < grid.length; r++)
+    {
+        for(var c=0; c <=3; c++)
+        {
+            
+            if(c !== 0  && grid[r][c] !== "x" && grid[r][c-1] === "x")
+            {
+                grid[r][c-1] = grid[r][c];
+                grid[r][c] = "x";
+           //     console.log(c);
+               //console.log(grid.length);
+            }
+            
+        }
+        
+    }   
+    
+}
+function runGame(){
+
+	//temporary function to demonstrate interaction between html and javascript
+	/*var board = [[2,4,8,16],[32,64,128,512],[1024,0,0,0],[0,0,0,0]];
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			var boardID = "r"+i+"c"+j;
+			if(board[i][j]!=0){
+				document.getElementById(boardID).innerHTML = board[i][j];
+			}
+		}
+	}*/
+setUpBoard();
+	//temporary function to demonstrate interaction between css and javascript
+	//you can implement a case switch here, if you want to you can, if not don't worry about it
+	//they're pretty intuitive to understand. 
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			var boardID = "r"+i+"c"+j;
+			if(board[i][j] == 2){
+				document.getElementById(boardID).style.background = "#f0e5da";
+			}
+			else if(board[i][j] == 4){
+				document.getElementById(boardID).style.background = "#ede2c8";
+			}
+			else if(board[i][j] == 8){
+				document.getElementById(boardID).style.background = "#feb578";
+			}
+			else if(board[i][j] == 16){
+				document.getElementById(boardID).style.background = "#ff9962";
+			}
+			else if(board[i][j] == 32){
+				document.getElementById(boardID).style.background = "#ff8060";
+			}
+			else if(board[i][j] == 64){
+				document.getElementById(boardID).style.background = "#ff613c";
+			}
+			else if(board[i][j] == 128){
+				document.getElementById(boardID).style.background = "#efd26d";
+			}
+			else if(board[i][j] == 256){
+				document.getElementById(boardID).style.background = "#efd15c";
+			}
+			else if(board[i][j] == 512){
+				document.getElementById(boardID).style.background = "#efcd4a";
+			}
+			else if(board[i][j] == 1024){
+				document.getElementById(boardID).style.background = "#f0ca36";
+			}
+			else if(board[i][j] == 2048){
+				document.getElementById(boardID).style.background = "#ccc0b3";
+			}
+			else{
+
+			}
+		}
+	}
+}
+
+//show students an ascii conversion tool. 
+function printKey(e){
+	console.log(e.keyCode);
+}
